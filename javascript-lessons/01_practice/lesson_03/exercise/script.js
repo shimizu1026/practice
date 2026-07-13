663094a1b3478aea11a384401711d4b0ddf0acb5
shimizu <nichibishimizu@gmail.com>
2026-07-13 16:08:49 +0900

    Javascriptレッスン3

---
+++ javascript-lessons/01_practice/lesson_03/exercise/script.js
@@ -0,0 +1,20 @@
+
+const isConfirmed = confirm("お振込しますか？");
+
+if (isConfirmed) {
+	const amount = prompt("お振り込み金額を入力");
+
+	if (!amount) {
+		alert("金額を入力してください");
+	}
+
+	if (amount < 1000) {
+		alert("1000円未満のお振込みはできません");
+	}
+
+	if (!amount && amount < 1000) {
+		alert(amount);
+	}
+} else {
+	alert("振込をキャンセルしました");
+}
--- javascript-lessons/01_practice/lesson_03/script.js
+++ javascript-lessons/01_practice/lesson_03/script.js
@@ -5,7 +5,36 @@ const isConfirmed = confirm("お食事をはじめますか");
 
 if (isConfirmed) {
 	console.log("OKが押されました");
+
+	const age = Number(prompt("年齢を入力してください。"))// promptは文字列で受け取る,数値型にするにはNumberに入れる
+
+	// 比較演算子
+	if (age >= 20) {
+		console.log("成人ですね")
+	}
+
+	if (age == 20) {
+		console.log("20歳ですね")
+	}
+
+	if (age === 20) {
+		console.log("20歳ですね")
+	}
+	// 	論理演算子
+	if (age >= 20 && age < 60) {
+		console.log("20歳以上60歳未満の方:5,000円");
+	}
+
+	if (age && (age < 20 || age >= 60)) {
+		console.log("20歳未満または、60歳以降の方:1,000円");
+	}
+
+	if (!age) {
+		console.log("年齢を入力していない方:100,000円");
+	}
 } else {
 	// falseの処理
 	console.log("キャンセルされました");
 }
+
+console.log(age);
