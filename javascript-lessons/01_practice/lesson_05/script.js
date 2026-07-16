3e5ccd68af076013106d7449344346bbfaa52cc0
shimizu <nichibishimizu@gmail.com>
2026-07-16 18:02:31 +0900

    Update script.js

---
+++ javascript-lessons/01_practice/lesson_05/script.js
@@ -0,0 +1,17 @@
+// 配列の作成
+const todos = ["メールを確認する", "買い物に行く", "掃除をする"];
+
+console.log(todos);
+
+// 要素のアクセス
+todos[1] = "おいしいご飯を食べる";
+console.log(todos[1]);
+
+// 要素の追加
+todos.push("お風呂に入る");//末尾に追加
+todos.shift();//先頭の削除
+
+// 配列の長さ
+console.log(todos.length);
+
+// 配列のループ
