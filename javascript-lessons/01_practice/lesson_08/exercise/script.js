ed71dbd1018f4434077c347e3cc701611441ebc4
shimizu <nichibishimizu@gmail.com>
2026-07-23 18:21:41 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_08/exercise/script.js
+++ javascript-lessons/01_practice/lesson_08/exercise/script.js
@@ -4,24 +4,26 @@ let timerId;
 let rafId;
 
 // 「1」〜「30」までを1秒ごとにコンソールに出力
-const countDown = () => {
-	console.log(`${counter}秒`);
+const timer = setInterval(() => {
+	console.log(counter);
 
 	if (counter >= 30) {
 		// 30まで数え終わった時、警告ダイアログで「オモロー」と表示する
 		alert("オモロー");
-		clearInterval(timerId);
+		clearInterval(timer);
 		return;
 	}
-}
+	counter++;
+}, 1000);
+
 
 // 上記数値を出力する際、数字が3の倍数のときは、（あほ）、5の倍数のときは、（犬） を数値の横に付けて出力する
 const nabeatu = () => {
-	if (counter % 3) {
+	if (counter % 3 === 0) {
 		console.log(`counter "あほ"`);
 	} else if (counter % 5 === 0) {
 		console.log(`counter "犬"`);
 	}
 }
-rafId = requestAnimationFrame();
+
 
