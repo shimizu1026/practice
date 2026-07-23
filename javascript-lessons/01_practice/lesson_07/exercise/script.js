d566788b87dc9042a60f20c0c391f28a5c0b3c99
shimizu <nichibishimizu@gmail.com>
2026-07-23 13:47:22 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_07/exercise/script.js
+++ javascript-lessons/01_practice/lesson_07/exercise/script.js
@@ -1,41 +1,25 @@
 // 「Chapter6」の練習問題で作成したコードの中で「関数に分けたほうがよい部分」を見つけて（絶対的な正解はない）、関数として定義し、使うように書き換えてる。（どの関数の宣言を使ってもよいこととする）
 
 // ポケモンオブジェクトの生成
-const pikachu = {
-	name: "ピカチュウ",
-	type: "でんき",
-	level: 1,
-};
+// const pikachu = {
+// 	name: "ピカチュウ",
+// 	type: "でんき",
+// 	level: 1,
+// };
 
-const hitokage = {
-	name: "ヒトカゲ",
-	type: "ほのお",
-	level: 5,
-};
+// const hitokage = {
+// 	name: "ヒトカゲ",
+// 	type: "ほのお",
+// 	level: 5,
+// };
 
-const zenigame = {
-	name: "ゼニガメ",
-	type: "みず",
-	level: 5,
-};
+// const zenigame = {
+// 	name: "ゼニガメ",
+// 	type: "みず",
+// 	level: 5,
+// };
 
 const catchPokemon = (pokemon) => {
-	const { name } = pokemon;
-	// ここに confirm / prompt / alert
-	confirm(`野生の${name}が現れた！捕まえますか？`);
-	// 捕まえたら pokemon を return
-
-	alert(`${name}を捕まえた！`);
-	// 捕まえなかったら null を return
-};
-
-// 手持ちのポケモンの管理用配列
-const party = [];
-
-// 野生のポケモンが現れたときの処理
-// ループを活用すれば以下のように書けます。
-const fieldPokemon = [pikachu, hitokage, zenigame];
-for (const pokemon of fieldPokemon) {
 	const { name } = pokemon;
 	if (confirm(`野生の${name}が現れた！捕まえますか？`)) {
 		// ニックネームをプロパティに追加
@@ -45,12 +29,41 @@ for (const pokemon of fieldPokemon) {
 		party.push(pokemon);
 
 		alert(`${name}を捕まえた！`);
+		return pokemon;
+	}
+	return null;
+}
+
+const displayParty = (party) => {
+	for (const pokemon of party) {
+		const { name, nickname, type, level } = pokemon;
+
+		console.log(`${name} | ${nickname || name} | ${type} | Lv ${level}`);
 	}
 }
 
-// 手持ちのポケモンの情報を表示
-for (const pokemon of party) {
-	const { name, nickname, type, level } = pokemon;
-
-	console.log(`${name} | ${nickname || name} | ${type} | Lv ${level}`);
+const createPokemon = (name, type, level) => {
+	return {
+		name: name,
+		type: type,
+		level: level,
+	};
 }
+
+const pikachu = createPokemon("ピカチュウ", "でんき", 1);
+const hitokage = createPokemon("ヒトカゲ", "ほのお", 5);
+const zenigame = createPokemon("ゼニガメ", "みず", 5);
+
+// 手持ちのポケモンの管理用配列
+const party = [];
+
+// 野生のポケモンが現れたときの処理
+// ループを活用すれば以下のように書けます。
+const fieldPokemon = [pikachu, hitokage, zenigame];
+for (const pokemon of fieldPokemon) {
+	catchPokemon(pokemon);
+}
+
+// 手持ちのポケモンの情報を表示
+
+displayParty(party);
