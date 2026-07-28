376159bb3ff5578c3fcd4c644ed11c999b918d1e
shimizu <nichibishimizu@gmail.com>
2026-07-28 12:00:57 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_10/script.js
+++ javascript-lessons/01_practice/lesson_10/script.js
@@ -1,14 +1,24 @@
 const todos = [
-  {
-    id: 1,
-    task: "買い物",
-    isDone: false,
-    priority: 2,
-  },
-  {
-    id: 2,
-    task: "掃除",
-    isDone: true,
-    priority: 1,
-  },
+	{
+		id: 1,
+		task: "買い物",
+		isDone: false,
+		priority: 2,
+	},
+	{
+		id: 2,
+		task: "掃除",
+		isDone: true,
+		priority: 1,
+	},
 ];
+
+// 新しいタスクを追加
+const newTask = prompt("タスクを入力");
+
+if (newTask) {
+
+	const newTodo = {
+		id: slice,
+	}
+}
