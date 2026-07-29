a18e4e1a582d773bc6038f64ac4e58221e579f37
shimizu <nichibishimizu@gmail.com>
2026-07-29 14:37:39 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_10/script.js
+++ javascript-lessons/01_practice/lesson_10/script.js
@@ -4,12 +4,14 @@ const todos = [
 		task: "買い物",
 		isDone: false,
 		priority: 2,
+		createdAt: "2025-04-01T16:00",
 	},
 	{
 		id: 2,
 		task: "掃除",
 		isDone: true,
 		priority: 1,
+		createdAt: "2025-05-24T10:30",
 	},
 ];
 
@@ -17,17 +19,33 @@ const todos = [
 const newTask = prompt("タスクを入力");
 
 if (newTask) {
-
+	const now = new Date();
+	console.log(now.toISOString());
 	const newTodo = {
 		id: todos.length + 1,
 		task: newTask,
 		isDone: false,
 		priority: 3,
+		createdAt: now.toISOString(),
 	}
 
 	todos.push(newTodo);
 };
 
+// 作成日順で並び替えた新しい配列を作成
+const sortedDescTodos = todos.slice().sort((a, b) => {
+	return new Date(b.createdAt) - new Date(a.createdAt);
+});
+sortedDescTodos.forEach((todo) => {
+	const { task, isDone, priority, createdAt } = todo;
+
+	// 作成日と日本のローカル時間でフォーマット
+	const formattedDate = new Date(createdAt).toLocaleString("ja-JP", {
+		timeZone: "Asia/Tokyo",//日本のタイムゾーン
+	});
+
+	console.log(`${task} | ${isDone} | ${priority}|${formattedDate}`);
+})
 // console.log(todos);
 
 // 逆順にした新しい配列をつくる
