5b0a984796310dd01fc085faab10ac0f7a05f32c
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2026-08-01 20:50:02 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_13/script.js
+++ javascript-lessons/01_practice/lesson_13/script.js
@@ -1,7 +1,33 @@
 //要素の取得
 const themeElem = document.querySelector("[data-theme]");
-
+const buttons = document.querySelectorAll("[data-button]");
+console.log(buttons);
 // テーマの切り替え
 themeElem.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+});
+
+//画像の切り替え
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // 操作対象の要素を取得
+    const currentTarget = event.currentTarget;
+    // console.log(currentTarget);
+    const currentTargetId = currentTarget.getAttribute("aria-controls");
+    // console.log(currentTargetId);
+
+    const imageElem = document.querySelector(`#${currentTargetId}`);
+    // console.log(imageElem);
+
+    // ボタン内の画像情報を取得
+    const target = event.target;
+    const src = target.getAttribute("src" || "");
+    const alt = target.getAttribute("alt" || "");
+
+    // console.log(`src:${src}|alt:${alt}`);
+
+    // 画像の情報を更新
+    imageElem.setAttribute("src", src);
+    imageElem.setAttribute("alt", alt);
+  });
 });
