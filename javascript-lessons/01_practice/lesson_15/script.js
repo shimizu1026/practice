b26e9576fb1afe7c9a9e0a6adeba9b2f46701ce0
shimizu <nichibishimizu@gmail.com>
2026-08-03 16:44:02 +0900

    Update script.js

---
+++ javascript-lessons/01_practice/lesson_15/script.js
@@ -0,0 +1,23 @@
+const todoData = [
+	{
+		title: "提案書の提出",
+		category: "仕事",
+		priority: "高",
+		dueDate: "2025-07-18",
+		isDone: false,
+	},
+	{
+		title: "ゲームの返却",
+		category: "プライベート",
+		priority: "中",
+		dueDate: "2025-07-20",
+		isDone: true,
+	},
+	{
+		title: "本を返却",
+		category: "プライベート",
+		priority: "低",
+		dueDate: "2025-07-25",
+		isDone: false,
+	},
+];
