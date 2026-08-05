1098252f223f8ac26b381d7ff6c41321eeb674c1
shimizu <nichibishimizu@gmail.com>
2026-08-05 16:27:22 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_16/script.js
+++ javascript-lessons/01_practice/lesson_16/script.js
@@ -1,7 +1,11 @@
 const drawer = document.querySelector("[data-drawer]");
 const openButton = document.querySelector('[data-command="show-modal"]');
-const closeButton = document.querySelector("[close]");
+const closeButton = document.querySelector('[data-command="close"]');
 
+// イベントリスナー
 openButton.addEventListener("click", () => {
 	drawer.showModal();
+})
+closeButton.addEventListener("click", () => {
+	drawer.close();
 })
