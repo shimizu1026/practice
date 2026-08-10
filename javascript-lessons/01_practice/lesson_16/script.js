1d7a17320cdbcd3788c420352b019c4efeb7d645
shimizu <nichibishimizu@gmail.com>
2026-08-10 10:01:19 +0900

    Update script.js

--- javascript-lessons/01_practice/lesson_16/script.js
+++ javascript-lessons/01_practice/lesson_16/script.js
@@ -12,8 +12,21 @@ closeButton.addEventListener("click", () => {
 
 // 背景クリック機能
 drawer.addEventListener("click", (event) => {
-	console.log(event.clientX);
-})
+	// console.log(event.clientX);
 
-// ダイアログの位置とサイズを取得
-const rect
+	// ダイアログの位置とサイズを取得
+	const rect = drawer.getBoundingClientRect();
+	// console.log(rect);
+
+	// クリック位置がダイアログの範囲外かどうかを判定
+	const isOutside =
+		event.clientX < rect.left || //マウスの位置がダイアログの左端より左
+		event.clientX > rect.right || //マウスの位置がダイアログの右端より右
+		event.clientY < rect.top || //マウスの位置がダイアログの上端より上
+		event.clientY > rect.bottom;//マウスの位置がダイアログの下端より下
+	// 範囲外の場合はダイアログを閉じる
+	if (isOutside) {
+		drawer.close("背景クリック");
+	}
+});
+
