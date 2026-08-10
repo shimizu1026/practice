9104fc36e9f84c46dda09810ef9246249f6b1e0c
shimizu <nichibishimizu@gmail.com>
2026-08-10 10:10:27 +0900

    Javascriptレッスン16

--- javascript-lessons/01_practice/lesson_16/exercise/script.js
+++ javascript-lessons/01_practice/lesson_16/exercise/script.js
@@ -2,8 +2,10 @@ const openButton = document.querySelector('[data-command="show-modal"]');
 const submitButton = document.querySelector('[data-command="submit"]');
 
 
-// 「入力確認」ボタンを押すと、入力内容をチェックし、エラーがなければ入力内容をモーダルウインドウに表示するようにする
+// 「入力確認」ボタンを押すと、入力内容をチェックし、エラーがなければ入力内容をモーダルウインドウに表示するようにす
+openButton.addEventListener("click", () => {
 
+})
 //  モーダルウインドウ内の「修正」ボタンを押すと、モーダルウインドウが閉じるようする
 
 //  モーダルウインドウ内の「送信」ボタンを押すと、入力内容を submit() で送信し、モーダルウインドウが閉じるようする
