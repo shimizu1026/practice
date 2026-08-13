ae875002ea0bd6ac4ab17ee592e0c05f2e7d2bb9
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2026-08-13 22:10:26 +0900

    Update script.js

---
+++ javascript-lessons/01_practice/lesson_18/script.js
@@ -0,0 +1,18 @@
+// 要素の取得
+const targets = document.querySelectorAll("[data-inview-trigger]");
+
+console.log(targets);
+
+// コールバック関数
+const handleAnimation = () => {
+  console.log("callback");
+};
+
+// オプション
+const options = {
+  rootMargin="-25% 0%",
+
+}
+
+// Intersection Observer(監視員)を作成
+const observer = new IntersectionObserver(handleAnimation, options);
